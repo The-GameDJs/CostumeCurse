9e7dc7d25c003b084a1eb353c69c1ffdcd68c827
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-10-28 18:46:27 -0400

    Add Initial Scene with Perspective
    Camera Rigging
    Player Movement
    Scripts for Player and Camera
    Imported Low Poly Assets for sampling

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+    public float height;
+    public float offsetX;
+    public float offsetZ;
+
+    void Update()
+    {
+        this.transform.position = player.transform.position + new Vector3(offsetX, height, offsetZ);
+    }
+}
