8ff7aeee25c7ef45aff858c4631db8313896ab74
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-10-31 20:42:01 -0400

    Add Enemy UI

--- Assets/Sandbox/Elizabeth/Scripts/Character/EnemyCharacter.cs
+++ Assets/Sandbox/Elizabeth/Scripts/Character/EnemyCharacter.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class EnemyCharacter : Character
 {
+    [SerializeField] 
+    private GameObject enemyPanel;
+    [SerializeField]
+    private GameObject enemyUITemplate;
+    private GameObject enemyUI;
+
+    private bool displayUI;
+
+    private void Update()
+    {
+        if (displayUI)
+        {
+            Vector3 relativeScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+            enemyUI.transform.position = relativeScreenPosition;
+        }
+    }
+
+    public void DisplayUI(bool displayUI)
+    {
+        this.displayUI = displayUI;
+        enemyUITemplate.SetActive(displayUI);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        enemyUI = Instantiate(enemyUITemplate);
+        enemyUI.transform.parent = enemyPanel.transform;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DisplayUI(true); // for now
     }
 
     public override void PlayTurn()
