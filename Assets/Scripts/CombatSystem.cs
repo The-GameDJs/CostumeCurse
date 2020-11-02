d3889d000272ffc6332e9e4dbe51c2c291543b41
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-11-01 20:08:48 -0500

    Temporary Work
    To be reverted after

---
+++ Assets/Scripts/CombatSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatSystem : MonoBehaviour
+{
+    private enum CombatState { Inactive, Start, End, Player, Enemy }
+    private enum PlayerTurn { Sield, Ganiel }
+
+    private CombatState combatState = CombatState.Inactive;
+    private PlayerTurn playerTurn = PlayerTurn.Sield;
+
+    [SerializeField]
+    private GameObject sieldGO;
+    private PlayableCharacter sield;
+    //[SerializeField]
+    //private GameObject ganiel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sield = sieldGO.GetComponent<PlayableCharacter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            InitiateCombat();
+
+        if (combatState == CombatState.Start)
+        {
+
+        }
+
+        if (combatState == CombatState.Player)
+        {
+            
+
+            // show UI of player
+        }
+
+        if (combatState == CombatState.Enemy)
+        {
+
+        }
+    }
+
+    /*
+     * Invokable by the CombatZone
+    */
+    public void InitiateCombat()
+    {
+        combatState = CombatState.Start;
+
+        Debug.Log("Start of our test");
+        Debug.Log($"Test concluded with result: {sield.ReturnablePlayTurn()}");
+        //sield.PlayTurn();
+        // ResultOfTurn resultOfTurn = sield.PlayTurn();
+    }
+}
--- Assets/Scripts/Costume.cs
+++ Assets/Scripts/Costume.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Costume : MonoBehaviour
 {
-    [SerializeField] private GameObject abilitiesUI;
+    [SerializeField] private GameObject playerUIPanel;
     private bool displayAbilities;
 
     // TODO: Stop displaying abilities UI after a choice
@@ -15,13 +15,13 @@ public class Costume : MonoBehaviour
         if (displayAbilities)
         {
             Vector3 relativeScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
-            abilitiesUI.transform.position = relativeScreenPosition;
+            playerUIPanel.transform.position = relativeScreenPosition;
         }
     }
 
     public void DisplayAbilities(bool displayAbilities)
     {
         this.displayAbilities = displayAbilities;
-        abilitiesUI.SetActive(displayAbilities);
+        playerUIPanel.SetActive(displayAbilities);
     }
 }
