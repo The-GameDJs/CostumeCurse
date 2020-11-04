fb505d6813161a6bf726a5b9d18f970a2a1a1ac1
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2020-11-04 16:39:58 -0500

    Make text bubble appear above NPC's head and fix regex bug

--- Assets/Scripts/InteractiveNPC.cs
+++ Assets/Scripts/InteractiveNPC.cs
@@ -19,7 +19,10 @@ public class InteractiveNPC : MonoBehaviour
     {
         if (displayDialogueBubble)
         {
-            Vector3 relativeScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+            // TODO: Maybe change the hardcoded value to something else?
+            var yOffset = transform.position.y + (GetComponent<Collider>().bounds.size.y) + 1.0f;
+            Vector3 offsetPos = new Vector3(transform.position.x, transform.position.y + yOffset, transform.position.z);
+            Vector3 relativeScreenPosition = Camera.main.WorldToScreenPoint(offsetPos);
             DialogueUI.transform.position = relativeScreenPosition;
         }
     }
