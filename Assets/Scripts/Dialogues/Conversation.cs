9a0b4f9a32dc2871f7cf7c5b212ad2d6635be7ee
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2020-11-15 23:31:15 -0500

    Create conversations system

---
+++ Assets/Scripts/Dialogues/Conversation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Conversation", menuName = "Conversation")]
+public class Conversation : ScriptableObject
+{
+
+}
