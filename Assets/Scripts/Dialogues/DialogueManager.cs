cf7ae3ff7336891231d6da607813b5817871ba95
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2020-11-16 23:15:39 -0500

    Create DialogueManager

---
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Start dialogue
+    void StartDialogue() 
+    {
+        // Display line will be handled by DialogueBubble
+    }
+
+    
+    void UpdateDialogueBubblePosition() 
+    {
+        // Get current speaker and make sure dialogue bubble is updated
+        // according to that speaker's position
+    }
+}
