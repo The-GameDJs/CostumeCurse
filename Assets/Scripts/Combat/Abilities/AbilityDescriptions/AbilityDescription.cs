9052329be0f5b14bd1b70e78ea931188c24380f7
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2020-11-30 03:45:14 -0500

    Create abilities menu base
    Add AbilitySelectHandler for animations
    
    Finish base abilities menu
    
    Update sandbox scene

---
+++ Assets/Scripts/Combat/Abilities/AbilityDescriptions/AbilityDescription.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ability Description", menuName = "Ability Description")]
+public class AbilityDescription : ScriptableObject
+{
+    public string Name;
+    [TextArea(2, 5)]
+    public string Description;
+}
---
+++ Assets/Scripts/Combat/AbilitySelectHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class AbilitySelectHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator AbilityAnim;
+    [SerializeField]
+    private AbilityDescription AbilityDescription;
+    [SerializeField]
+    private GameObject AbilityDescriptionPanel;
+    private TMP_Text Name;
+    private TMP_Text Description;
+
+    void Start()
+    {
+        AbilityDescriptionPanel.SetActive(false);
+        AbilityAnim = GetComponent<Animator>();
+        Name = AbilityDescriptionPanel.GetComponentsInChildren<TMP_Text>()[0];
+        Description = AbilityDescriptionPanel.GetComponentsInChildren<TMP_Text>()[1];
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AbilityAnim.SetBool("isSelected", true);
+        Name.text = AbilityDescription.Name;
+        Description.text = AbilityDescription.Description;
+        AbilityDescriptionPanel.SetActive(true);
+    }
+ 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        AbilityAnim.SetBool("isSelected", false);
+        AbilityDescriptionPanel.SetActive(false);
+    }
+}
