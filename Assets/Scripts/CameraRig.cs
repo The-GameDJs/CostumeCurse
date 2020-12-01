85599fb381c8bf7910355f11e08114f54076f272
Daniel <danielwiktorczyk@gmail.com>
2020-12-01 14:43:56 -0500

    Focus In with Camera Transitions
    Add Camera Transitions
    Add Camera Zones to Key Areas
    Add Player Movement in Camera Direction
    Add Camera Transition in Combat
    Generalize Camera Movement
    Update Editor Params for Camera Movement
    
    Authored-by: Armando Russo <russo_449@hotmail.com>
    Co-authored-by: Armando Russo <russo_449@hotmail.com>

--- Assets/Scripts/CameraRig.cs
+++ Assets/Scripts/CameraRig.cs
@@ -4,13 +4,140 @@ using UnityEngine;
 
 public class CameraRig : MonoBehaviour
 {
-    public GameObject player;
-    public float height;
-    public float offsetX;
-    public float offsetZ;
+    private enum CameraPhase { InTransition, NoTransition };
+    private CameraPhase CurrentPhase;
+    private Camera MainCamera;
+    
+    private Timer Timer;
 
-    void Update()
+    // Current
+    private bool IsRelativeToTargetGO;
+    public float Smoothness;
+
+    // Target (end of transition)
+    private Vector3 TargetPosition;
+    private Vector3 TargetOffset;
+    private GameObject TargetGO;
+    private Quaternion TargetRotation;
+
+    // Initial (before transition)
+    private Vector3 InitialPosition;
+    private Quaternion InitialRotation;
+
+
+    // Defaults
+    [SerializeField]
+    public Vector3 DefaultOffset;
+    public Quaternion DefaultRotation =
+        Quaternion.identity;
+    [SerializeField]
+    private readonly float DefaultSmoothness = 5f;
+
+    private void Start()
     {
-        this.transform.position = player.transform.position + new Vector3(offsetX, height, offsetZ);
+        IsRelativeToTargetGO = true;
+        TargetGO = GameObject.Find("Sield");
+        TargetOffset = DefaultOffset;
+        InitialPosition = TargetPosition = TargetOffset + TargetGO.transform.position;
+        InitialRotation = TargetRotation = DefaultRotation;
+        this.transform.position = InitialPosition;
+        this.transform.rotation = InitialRotation;
+        Smoothness = DefaultSmoothness;
+
+        Timer = GetComponent<Timer>();
+        MainCamera = GetComponent<Camera>();
+        CurrentPhase = CameraPhase.NoTransition;
+    }
+
+    // Moves independent to the CurrentGameObject
+    public void MoveCameraAbsolute(Vector3 targetPosition, Quaternion targetRotation)
+    {
+        IsRelativeToTargetGO = false;
+
+        InitialPosition = this.transform.position;
+        InitialRotation = this.transform.rotation;
+
+        TargetPosition = targetPosition;
+        TargetRotation = targetRotation;
+
+        CurrentPhase = CameraPhase.InTransition;
+        Timer.StartTimer(Smoothness);
+    }
+
+    // Moves relative to the CurrentGameObject
+    public void MoveCameraRelative(Vector3 targetOffset, Quaternion targetRotation)
+    {
+        IsRelativeToTargetGO = true;
+        
+        InitialPosition = this.transform.position;
+        InitialRotation = this.transform.rotation;
+
+        TargetOffset = targetOffset;
+        TargetRotation = targetRotation;
+
+        CurrentPhase = CameraPhase.InTransition;
+        Timer.StartTimer(Smoothness);
+    }
+
+    public void SetTargetGO(GameObject targetObject)
+    {
+        TargetGO = targetObject;
+    }
+
+    public void SetTransitionSmoothness(float transitionSmoothness)
+    {
+        Smoothness = transitionSmoothness;
+    }
+
+    void LateUpdate()
+    {
+        switch(CurrentPhase)
+        {
+            case CameraPhase.NoTransition:
+                NoTransitionUpdate();
+                break;
+
+            case CameraPhase.InTransition:
+                InTransitionUpdate();
+                break;
+        }
+
+    }
+
+    private void InTransitionUpdate()
+    {
+        if (Timer.IsInProgress())
+        {
+            TargetPosition = IsRelativeToTargetGO ?
+                TargetGO.transform.position + TargetOffset :
+                TargetPosition;
+
+            this.transform.position = Vector3.Lerp(InitialPosition,
+                TargetPosition,
+                Timer.GetProgress() / Smoothness);
+            this.transform.rotation = Quaternion.Lerp(InitialRotation,
+                TargetRotation,
+                Timer.GetProgress() / Smoothness);
+        }
+        else
+        {
+            Debug.Log($"Done! {Timer.GetProgress()}");
+
+            Timer.ResetTimer();
+
+            CurrentPhase = CameraPhase.NoTransition;
+
+            Smoothness = DefaultSmoothness; // revert transitional smoothness to the default
+        }
+    }
+
+    private void NoTransitionUpdate()
+    {
+        TargetPosition = IsRelativeToTargetGO ?
+                        TargetGO.transform.position + TargetOffset :
+                        TargetPosition;
+
+        this.transform.position = TargetPosition;
+        this.transform.rotation = TargetRotation;
     }
 }
