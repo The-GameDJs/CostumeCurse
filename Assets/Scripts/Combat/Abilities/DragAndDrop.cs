26ebff8ca50f44b9995b6cbca0b3c07928fe9b59
Melina Phan <phan.melina@gmail.com>
2020-12-01 15:23:13 -0500

    Add cooking ability
    start of slider phase
    
    mechanisms of cooking-ability complete
    
    damage to enemies completed
    
    Pullrequest comment fixes

---
+++ Assets/Scripts/Combat/Abilities/DragAndDrop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    
+    private Vector3 OriginalPos;
+    private bool IsInside = false;
+    private RectTransform RectTransform;
+    private CanvasGroup CanvasGroup;
+
+    private void Start()
+    {
+        RectTransform = GetComponent<RectTransform>();
+        CanvasGroup = GetComponent<CanvasGroup>();
+        OriginalPos = RectTransform.localPosition;
+    }
+
+    public void ResetPosition()
+    {
+        RectTransform.localPosition = OriginalPos;
+        IsInside = false;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        CanvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // TODO: possibly snap back to original position ?
+        if(!IsInside) // for sweets/rots
+            RectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        CanvasGroup.blocksRaycasts = true;
+    }
+
+    public void SetIsInside(bool isInsideSlot)
+    {
+        IsInside = isInsideSlot;
+    }
+
+    public bool GetIsInside()
+    {
+        return IsInside;
+    }
+}
---
+++ Assets/Scripts/Combat/Abilities/WheelHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WheelHandler : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private RectTransform RectTransform;
+    private Quaternion StartRotation;
+    private Quaternion LastRotation;
+    float LastAngle;
+    bool WasDragged;
+    bool FullRotation;
+    int Rotations;
+    
+    private void Start() 
+    {
+        RectTransform = GetComponent<RectTransform>();
+        StartRotation = RectTransform.rotation;
+        FullRotation = false;
+        Rotations = -1;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector2 dir = Input.mousePosition - RectTransform.position;
+        LastRotation = RectTransform.rotation;
+        LastAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        WasDragged = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //TODO: disable going counter-clockwise
+        Vector2 dir = Input.mousePosition - RectTransform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        RectTransform.rotation = LastRotation * Quaternion.AngleAxis(angle - LastAngle, Vector3.forward);
+        Debug.Log(RectTransform.rotation);
+        if(RectTransform.rotation.z <= 0)
+        {
+            FullRotation = false;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        WasDragged = false;
+    }
+
+    private void IncrementRotation()
+    {
+        if(!FullRotation)
+        {
+            Rotations++;
+            FullRotation = true;
+        }
+    }
+}
