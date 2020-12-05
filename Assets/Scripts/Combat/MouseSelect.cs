db20176569a29283c9faff1404a3b52c9e02a9e4
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2020-12-05 04:36:47 -0500

    Implement single target selection

---
+++ Assets/Scripts/Combat/MouseSelect.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSelect : MonoBehaviour
+{
+    public GameObject SelectedObject;
+    private TargetSelector TargetSelector;
+    public LayerMask LayerMask;
+    public bool IsTargetSelected;
+
+    void Start()
+    {
+        IsTargetSelected = false;
+        TargetSelector = GetComponent<TargetSelector>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, LayerMask)) {
+            GameObject hitObject = hit.transform.gameObject;
+            SelectTarget(hitObject);
+        }
+        else 
+        {
+            ClearSelection();
+        }
+
+        if (SelectedObject != null && Input.GetButtonDown("Action Command"))
+        {
+            Debug.Log("Selected target");
+            TargetSelector.ChooseTarget(SelectedObject);
+            IsTargetSelected = true;
+        }
+    }
+
+    void SelectTarget(GameObject obj)
+    {
+        if (SelectedObject != null)
+        {
+            if (obj == SelectedObject)
+                return;
+
+            ClearSelection();
+        }
+
+        SelectedObject = obj;
+    }
+
+    void ClearSelection()
+    {
+        SelectedObject = null;
+    }
+}
---
+++ Assets/Scripts/Combat/TargetIndicator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    private MouseSelect MouseSelector;
+    private const float IndicatorScaler = 1.25f;
+
+    void Start()
+    {
+        MouseSelector = GameObject.FindGameObjectWithTag("TargetSelector").GetComponent<MouseSelect>();
+    }
+
+    void Update()
+    {
+        if(MouseSelector.SelectedObject != null) {
+			Bounds objBounds = MouseSelector.SelectedObject.GetComponent<Collider>().bounds;
+
+			transform.position = new Vector3(objBounds.center.x, MouseSelector.SelectedObject.transform.position.y, objBounds.center.z);
+			transform.localScale = objBounds.size * IndicatorScaler;
+		}
+    }
+}
