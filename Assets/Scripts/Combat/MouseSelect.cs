52800757c2a3895e0ec6c5c097c5827aeb329ab4
Elizabeth Lam <animew.cccl@gmail.com>
2020-12-10 23:02:28 -0500

    Update target selection visuals

--- Assets/Scripts/Combat/MouseSelect.cs
+++ Assets/Scripts/Combat/MouseSelect.cs
@@ -10,10 +10,13 @@ public class MouseSelect : MonoBehaviour
     public bool IsTargetSelected = false;
     public bool IsRegrettingDecision = false;
     public bool IsSingleTargetting = false;
+    private GameObject TargetIndicator;
 
     void Start()
     {
         TargetSelector = GetComponent<TargetSelector>();
+        TargetIndicator = GameObject.Find("TargetIndicator");
+        TargetIndicator.SetActive(false);
     }
 
     void Update()
@@ -25,6 +28,7 @@ public class MouseSelect : MonoBehaviour
         if (Physics.Raycast(ray, out hit, 100f, LayerMask)) {
             GameObject hitObject = hit.transform.gameObject;
             SelectTarget(hitObject);
+            TargetIndicator.SetActive(true);
         }
         else 
         {
@@ -34,11 +38,19 @@ public class MouseSelect : MonoBehaviour
         if (Input.GetButtonDown("Fast Forward"))
         {
             IsRegrettingDecision = true;
+            TargetIndicator.SetActive(false);
         }
 
         if (SelectedObject != null && Input.GetButtonDown("Action Command"))
         {
             Debug.Log("Selected target");
+            TargetIndicator.SetActive(false);
+            Renderer[] renderers = SelectedObject.GetComponentsInChildren<Renderer>();
+            foreach(Renderer r in renderers) {
+                Material m = r.material;
+                m.color = Color.white;
+                r.material = m;
+            }
             
             if (IsSingleTargetting)
                 TargetSelector.ChooseTarget(SelectedObject);
@@ -60,11 +72,31 @@ public class MouseSelect : MonoBehaviour
             }
 
             SelectedObject = target;
+
+            Renderer[] renderers = SelectedObject.GetComponentsInChildren<Renderer>();
+            foreach(Renderer r in renderers) {
+                Material m = r.material;
+                m.color = Color.red;
+                r.material = m;
+            }
         }
     }
 
     void ClearSelection()
     {
+        if(SelectedObject == null)
+        {
+            TargetIndicator.SetActive(false);
+            return;
+        }
+
+        Renderer[] renderers = SelectedObject.GetComponentsInChildren<Renderer>();
+		foreach(Renderer r in renderers) {
+			Material m = r.material;
+			m.color = Color.white;
+			r.material = m;
+		}
+
         SelectedObject = null;
     }
 }
