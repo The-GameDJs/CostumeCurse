b8f28aed913de2595c3e39fcc4b6e4213360e2b5
Armando Russo <russo_449@hotmail.com>
2021-01-22 22:01:22 -0500

    Start Revolver Ability

---
+++ Assets/Scripts/Combat/Abilities/Revolver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver : Ability
+{
+    private Timer Timer;
+    private enum RevolverPhase { Load, Shoot, Inactive }
+    private RevolverPhase CurrentPhase = RevolverPhase.Inactive;
+
+    [SerializeField] GameObject Bullet;
+    [SerializeField] Canvas ReloadCanvas;
+
+    public new void Start()
+    {
+        base.Start();
+        Timer = GetComponent<Timer>();
+        ReloadCanvas.gameObject.SetActive(false);
+
+        TargetSchema = new TargetSchema(
+            0,
+            CombatantType.Enemy,
+            SelectorType.Number);
+    }
+
+    public new void StartAbility(bool userTargeting = false)
+    {
+        base.StartAbility();
+        Debug.Log("Started Revolver Ability");
+    }
+
+    private void Update()
+    {
+        if (CurrentPhase == RevolverPhase.Load)
+            RevolverLoadUpdate();
+
+        if (CurrentPhase == RevolverPhase.Shoot)
+            RevolverShootUpdate();
+    }
+
+    protected override void ContinueAbilityAfterTargeting()
+    {
+        StartReloadPhase();
+    }
+
+    private void StartReloadPhase()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RevolverShootUpdate()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StartShootingPhase()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RevolverLoadUpdate()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void EndAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+}
