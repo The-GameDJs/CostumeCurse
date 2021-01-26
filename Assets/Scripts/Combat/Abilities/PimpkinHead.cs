ae4f6dc73b86800836517e5276cc40f2f7808671
Armando Russo <russo_449@hotmail.com>
2021-01-25 23:05:09 -0500

    Start Phase 2 of Revolver & Refactor Some Code

---
+++ Assets/Scripts/Combat/Abilities/PimpkinHead.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PimpkinHead : MonoBehaviour
+{
+
+    private bool IsHit;
+    private RectTransform PimpkinBody;
+
+
+    void Start()
+    {
+        IsHit = false;
+        PimpkinBody = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        /*    
+        RectTransform mrect = GetComponent<RectTransform>();
+        Vector2 apos = mrect.anchoredPosition;
+        float xpos = apos.x;
+        xpos = Mathf.Clamp(xpos, 0, Screen.width - mrect.sizeDelta.x);
+        apos.x = xpos;
+        mrect.anchoredPosition = apos;
+        */
+    }
+
+    public void DestroyPimpkin()
+    {
+        gameObject.SetActive(false);
+        IsHit = true;
+    }
+
+    public void ResetPimpkinValues()
+    {
+        IsHit = false;
+    }
+}
--- Assets/Scripts/Combat/Abilities/Revolver.cs
+++ Assets/Scripts/Combat/Abilities/Revolver.cs
@@ -6,26 +6,45 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
+using System.Threading;
 
 public class Revolver : Ability
 {
     private Timer Timer;
-    private float ReloadDuration = 4f;
-    private bool[] IsReserved;
+    private float ReloadDuration = 5f;
+    private float ShootingDuration = 5f;
+    private int TotalBulletsDropped = 0;
+    private bool[] IsBulletReserved;
+    private bool[] IsPimpkinReserved;
+
     private enum RevolverPhase { Load, Shoot, Inactive }
     private RevolverPhase CurrentPhase = RevolverPhase.Inactive;
     private Stack<DragAndDrop> BulletStuff = new Stack<DragAndDrop>();
     private Text ReloadTimerText;
 
+    [Header("Reload Phase")]
     [SerializeField] Canvas ReloadCanvas;
     [SerializeField] GameObject[] BulletPositions;
+    [SerializeField] GameObject[] Clips;
+
+    [Header("Shooting Phase")]
+    [SerializeField] Canvas ShootingCanvas;
+    [SerializeField] GameObject[] BulletUI;
+    [SerializeField] GameObject[] PimpkinSpawnLocations;
+    private Stack<PimpkinHead> Pimpkins = new Stack<PimpkinHead>();
+
 
     public new void Start()
     {
         base.Start();
         Timer = GetComponent<Timer>();
         ReloadCanvas.gameObject.SetActive(false);
-        IsReserved = new bool[BulletPositions.Length];
+        ShootingCanvas.gameObject.SetActive(false);
+        IsBulletReserved = new bool[BulletPositions.Length];
+        IsPimpkinReserved = new bool[PimpkinSpawnLocations.Length];
+
+        for (int i = 0; i < BulletUI.Length; i++)
+            BulletUI[i].SetActive(false);
 
         TargetSchema = new TargetSchema(
             1,
@@ -56,28 +75,32 @@ public class Revolver : Ability
     private void StartReloadPhase()
     {
         DragAndDrop[] bullets = ReloadCanvas.GetComponentsInChildren<DragAndDrop>();
-
-        foreach (DragAndDrop bullet in bullets)
-            BulletStuff.Push(bullet);
-
-        while(BulletStuff.Count != 0)
-        {
-            int randomPosition = Random.Range(0, 8);
-
-            if (!IsReserved[randomPosition])
-            {
-                DragAndDrop bullet = BulletStuff.Pop();
-                bullet.gameObject.transform.position = BulletPositions[randomPosition].transform.position;
-                bullet.InitializeStartingPosition();
-                IsReserved[randomPosition] = true;
-            }
-        }
+        ReloadCanvas.transform.position = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+        SetBulletLocations(bullets);
 
         ReloadCanvas.gameObject.SetActive(true);
         ReloadTimerText = GameObject.Find("ReloadTimerText").GetComponent<Text>();
         CurrentPhase = RevolverPhase.Load;
-        ReloadCanvas.transform.position = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
         Timer.StartTimer(ReloadDuration);
+    }
+
+    private void SetBulletLocations(DragAndDrop[] bullets)
+    {
+        foreach (DragAndDrop bullet in bullets)
+            BulletStuff.Push(bullet);
+
+        while (BulletStuff.Count != 0)
+        {
+            int randomPosition = Random.Range(0, 8);
+
+            if (!IsBulletReserved[randomPosition])
+            {
+                DragAndDrop bullet = BulletStuff.Pop();
+                bullet.gameObject.transform.position = BulletPositions[randomPosition].transform.position;
+                bullet.InitializeStartingPosition();
+                IsBulletReserved[randomPosition] = true;
+            }
+        }
     }
 
     private void ReloadUpdate()
@@ -89,24 +112,70 @@ public class Revolver : Ability
         }
 
         if(Timer.IsFinished())
-        {
             EndReloadPhase();
-        }
     }
 
     private void EndReloadPhase()
     {
-        // TODO: Calculate number of bullets dropped
+        Timer.ResetTimer();
+        ReloadCanvas.gameObject.SetActive(false);
+
+        ResetReloadValues();
+
+        Debug.Log($"Total bullets dropped: {TotalBulletsDropped}");
+        Thread.Sleep(500);
+        StartShootingPhase();
+    }
+
+    private void ResetReloadValues()
+    {
+        foreach (GameObject c in Clips)
+        {
+            Clip singleClip = c.GetComponent<Clip>();
+            if (singleClip.IsClipFilled())
+            {
+                TotalBulletsDropped++;
+            }
+
+            singleClip.ResetRevolverValues();
+        }
+
+        for (int i = 0; i < IsBulletReserved.Length; i++)
+            IsBulletReserved[i] = false;
     }
 
     private void StartShootingPhase()
     {
-        throw new NotImplementedException();
+        ShootingCanvas.gameObject.SetActive(true);
+        ShootingCanvas.transform.position = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+
+        for(int i = 0; i < TotalBulletsDropped; i++)
+            BulletUI[i].SetActive(true);
+
+        PimpkinHead[] pimpkins = ShootingCanvas.GetComponentsInChildren<PimpkinHead>();
+
+        foreach (PimpkinHead pimpkin in pimpkins)
+            Pimpkins.Push(pimpkin);
+
+        while (Pimpkins.Count != 0)
+        {
+            int randomPosition = Random.Range(0, 6);
+
+            if (!IsPimpkinReserved[randomPosition])
+            {
+                PimpkinHead pimpkin = Pimpkins.Pop();
+                pimpkin.gameObject.transform.position = PimpkinSpawnLocations[randomPosition].transform.position;
+                IsPimpkinReserved[randomPosition] = true;
+            }
+        }
+
+        CurrentPhase = RevolverPhase.Shoot;
+        Timer.StartTimer(ShootingDuration);
     }
 
     private void ShootUpdate()
     {
-        throw new NotImplementedException();
+        
     }
 
     protected override void EndAbility()
