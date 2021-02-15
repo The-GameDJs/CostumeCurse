74c3200f0c096a64642559b29f061227f4ec0166
George Mavroeidis <gdmavroeidis@hotmail.com>
2021-02-14 19:58:50 -0500

    Add Debug Editor Window Template
    using the UI Builder Package

---
+++ Assets/Scripts/Editor/DebugEditorWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+public class DebugEditorWindow : EditorWindow
+{
+    [MenuItem("Tools/Debug Menu _%#D")]
+    private static void ShowWindow()
+    {
+        var window = GetWindow<DebugEditorWindow>();
+        window.titleContent = new GUIContent("Debug Menu");
+        window.minSize = new Vector2(350, 350);
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        InitializeUxmlTemplate();
+    }
+
+    private void InitializeUxmlTemplate()
+    {
+        rootVisualElement.Clear();
+        var template = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Editor/DebugEditorView.uxml");
+        template.CloneTree(rootVisualElement);
+    }
+}
