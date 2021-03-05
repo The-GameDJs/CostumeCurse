c3edf2ca35ddb19eda7721c768422367fcc2dfac
Armando Russo <russo_449@hotmail.com>
2021-03-04 21:00:28 -0500

    Start Pimpkin Ability

---
+++ Assets/Scripts/Combat/Enemy Abilities/Supercharge.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Supercharge : Ability
+{
+    protected override void ContinueAbilityAfterTargeting()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void EndAbility()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
