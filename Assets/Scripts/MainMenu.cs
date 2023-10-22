9ea7af57caeb0e44dc625d67f3f321887bd4a0a8
George Mavroeidis <gdmavroeidis@hotmail.com>
2023-10-22 17:44:59 -0400

    Complete new Title Screen background scene with new credits

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    private GameObject CreditsPanel;
+    [SerializeField] private GameObject Crossfade;
+    [SerializeField] private Animator CrossfadeAnimator;
+    [SerializeField] private GameObject CreditsPanel;
+    [SerializeField] private RectTransform CreditsPanelRectTransform;
+    [SerializeField] private GameObject PlayButton;
+    [SerializeField] private GameObject CreditsButton;
+    [SerializeField] private GameObject QuitButton;
+    [SerializeField] private GameObject CloseCreditsButton;
+    [SerializeField] private GameObject TitleText;
+    [SerializeField] private float ScrollSpeed;
+    private bool AreCreditsRolling;
+    private Vector3 InitialCreditsPanelRectPosition;
+    
 
     void Start()
     {
-        CreditsPanel = GameObject.Find("Credits");
-        CreditsPanel.SetActive(false);
+        InitialCreditsPanelRectPosition = CreditsPanelRectTransform.position;
     }
 
+    private void Update()
+    {
+        if (AreCreditsRolling && CreditsPanelRectTransform.localPosition.y <= 1600.0f)
+        {
+            CreditsPanelRectTransform.position += new Vector3(0.0f, Time.deltaTime * ScrollSpeed, 0.0f);
+        }
+    }
 
     public void PlayGame()
     {
+        SceneManager.LoadScene("Prologue_Scene");
+    }
+
+    private IEnumerator LoadUpPrologueScene()
+    {
+        Crossfade.SetActive(true);
+        CrossfadeAnimator.SetTrigger("Start");
+        yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene("Prologue_Scene");
     }
 
@@ -26,6 +55,25 @@ public class MainMenu : MonoBehaviour
 
     public void ToggleCredits()
     {
-        CreditsPanel.SetActive(!CreditsPanel.active);
+        CreditsPanel.SetActive(true);
+        AreCreditsRolling = true;
+        PlayButton.SetActive(false);
+        CreditsButton.SetActive(false);
+        QuitButton.SetActive(false);
+        TitleText.SetActive(false);
+        CloseCreditsButton.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        PlayButton.SetActive(true);
+        CreditsButton.SetActive(true);
+        QuitButton.SetActive(true);
+        AreCreditsRolling = false;
+        CreditsPanelRectTransform.position = InitialCreditsPanelRectPosition;
+        CreditsPanel.SetActive(false);
+        TitleText.SetActive(true);
+        
+        CloseCreditsButton.SetActive(false);
     }
 }
--- Assets/Scripts/Utilities/ParticleSystemHelper.cs
+++ Assets/Scripts/Utilities/ParticleSystemHelper.cs
@@ -6,6 +6,7 @@ public class ParticleSystemHelper : MonoBehaviour
 {
     [SerializeField] private float startTime = 2.0f; // Adjust the start time as needed
     [SerializeField] private ParticleSystem particleSystem;
+    [SerializeField] private float ScrollSpeed;
 
     private void Start()
     {
