a130140a97a13fee7994af69d998adb89c286c8f
George Mavroeidis <gdmavroeidis@hotmail.com>
2023-10-27 02:24:29 -0400

    Remove unnecessary code that caused errors

--- Assets/Scripts/Combat/Abilities/Revolver.cs
+++ Assets/Scripts/Combat/Abilities/Revolver.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Combat.Abilities;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
